e8f33181c1b5cf572b5aebec4251efd077722bc2
Sugimoto Masayoshi <sugimoto.massayoshi@gmail.com>
2016-12-06 22:32:34 +0900

    Add Keyboard object.

---
+++ src/core/Keyboard.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const KEYBOARD_KEY_E      = 87;
+const KEYBOARD_KEY_A      = 65;
+const KEYBOARD_KEY_D      = 68;
+const KEYBOARD_KEY_SPACE  = 32;
+
+const Keyboard = {
+
+	setup(gameContext) {
+
+		//Set the handles when a key is pressed
+		window.onkeydown = function(event) {
+			switch (event.which) {
+				case KEYBOARD_KEY_E:
+					gameContext.ship.isBoost = true;
+					break;
+				case KEYBOARD_KEY_A:
+					gameContext.ship.isLeft = true;
+					break;
+				case KEYBOARD_KEY_D:
+					gameContext.ship.isRight = true;
+					break;
+			}
+		}
+
+		//Set key up handler
+		window.onkeyup = function(event) {
+			switch (event.which) {
+				case KEYBOARD_KEY_E:
+					gameContext.ship.isBoost = false;
+					break;
+				case KEYBOARD_KEY_A:
+					gameContext.ship.isLeft = false;
+					break;
+				case KEYBOARD_KEY_D:
+					gameContext.ship.isRight = false;
+					break;
+			}
+		}
+
+	}
+
+};
