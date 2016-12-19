d102882def9901963367d8072dfdcffcf6324811
Sugimoto Masayoshi <sugimoto.massayoshi@gmail.com>
2016-12-19 22:16:21 +0900

    Add SimpleEnemy class

---
+++ src/game/SimpleEnemy.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const SIMPLE_ENEMY_MAX_ROTATION_SPEED_RADIAN_SECOND = Math.PI; //Radian per second
+const SIMPLE_ENEMY_VELOCITY_METER_SECOND = 1; //Meter per second
+const SIMPLE_ENEMY_FIRE_RATE = 10; //Bullets per second
+
+const SimpleEnemy = {
+
+	create(target, machineGun, drawObject) {
+
+		let enemy = {
+			position: Vector2D.zero(),
+			direction: 0, //Radian
+			target: target,
+			drawObject: drawObject,
+			machineGun: machineGun
+		};
+
+		let proto = {
+
+			newDirection(elapsedTime) {
+				let me2Target = target.getPosition().substract(this.getPosition());
+
+				//Check if the distance is close to zero. In that case, no rotation.
+				if (Math.abs(me2Target.distance()) <= EPSILON) {
+					return this.getDirection();
+				}
+
+				//Rotate in the direction of the target
+				let deltaDirection =
+					me2Target.getAngle().substract(Angle.create(this.getDirection())).get();
+
+				//Within the max rotation
+				let maxRotation = SIMPLE_ENEMY_MAX_ROTATION_SPEED_RADIAN_SECOND * elapsedTime;
+				deltaDirection = Math.min(maxRotation, Math.max(deltaDirection, -maxRotation));
+
+				//Convert to angle
+				return Angle.create(this.getDirection() + deltaDirection).get();
+			},
+
+			newPosition(elapsedTime, newDirection) {
+				return this.getPosition().add(
+					Vector2D.unitX().rotate(newDirection) //Apply rotation
+						//Apply velocity
+						.scalarMultiply(SIMPLE_ENEMY_VELOCITY_METER_SECOND * elapsedTime));
+			},
+
+			update(elapsedTime) {
+
+				//Follow the target (Update rotation first)
+				let newDirection = this.newDirection(elapsedTime);
+				this.setDirection(newDirection);
+
+				//Follow the target (Update position)
+				this.setPosition(this.newPosition(elapsedTime, newDirection));
+
+				//Fire
+			},
+
+			draw(canvasContext) {
+				this.drawObject.draw(canvasContext);
+			}
+
+		};
+
+		return Object.assign(enemy, proto, GameSpacePositionableComposite(enemy));
+
+	},
+
+};
---
+++ src/game/tests/TestSimpleEnemy.js
@@ -0,0 +1,116 @@
+"use strict";
+
+const TestSimpleEnemy = {
+	util: Util.create()
+};
+
+{ //Test newDirection max rotation on left side
+	let target = {
+		getPosition() {
+			return Vector2D.create(1.0, -2.0); //Target is just below the enemy
+		}
+	};
+
+	let machineGun = {};
+	let drawObject = {};
+
+	let enemy = SimpleEnemy.create(target, machineGun, drawObject);
+
+	enemy.setPosition(Vector2D.create(1.0, 2.0));
+	enemy.setDirection(0.6 * Math.PI);
+
+	TestSimpleEnemy.util.assertEqualFloat(
+		-0.9 * Math.PI,
+		enemy.newDirection(0.5 /* elapsed time in second */));
+}
+
+{ //Test newDirection max rotation on right side
+	let target = {
+		getPosition() {
+			return Vector2D.create(1.0, -2.0); //Target is just below the enemy
+		}
+	};
+
+	let machineGun = {};
+	let drawObject = {};
+
+	let enemy = SimpleEnemy.create(target, machineGun, drawObject);
+
+	enemy.setPosition(Vector2D.create(1.0, 2.0));
+	enemy.setDirection(0.4 * Math.PI);
+
+	TestSimpleEnemy.util.assertEqualFloat(
+		-0.1 * Math.PI,
+		enemy.newDirection(0.5 /* elapsed time in second */));
+}
+
+{ //Test newDirection small rotation
+	let target = {
+		getPosition() {
+			return Vector2D.create(1.0, -2.0); //Target is just below the enemy
+		}
+	};
+
+	let machineGun = {};
+	let drawObject = {};
+
+	let enemy = SimpleEnemy.create(target, machineGun, drawObject);
+
+	enemy.setPosition(Vector2D.create(1.0, 2.0));
+	enemy.setDirection(0.4 * Math.PI);
+
+	TestSimpleEnemy.util.assertEqualFloat(
+		-0.5 * Math.PI,
+		enemy.newDirection(2.0 /* elapsed time in second */));
+}
+
+{ //Test newDirection close to the target
+	let target = {
+		getPosition() {
+			return Vector2D.create(1.0, 2.0); //Target is just below the enemy
+		}
+	};
+
+	let machineGun = {};
+	let drawObject = {};
+
+	let enemy = SimpleEnemy.create(target, machineGun, drawObject);
+
+	enemy.setPosition(Vector2D.create(1.0, 2.0));
+	enemy.setDirection(0.4 * Math.PI);
+
+	TestSimpleEnemy.util.assertEqualFloat(
+		0.4 * Math.PI,
+		enemy.newDirection(2.0 /* elapsed time in second */));
+}
+
+{ //Test newPosition
+	let target = {};
+	let machineGun = {};
+	let drawObject = {};
+
+	let enemy = SimpleEnemy.create(target, machineGun, drawObject);
+	enemy.setPosition(Vector2D.create(1.0, 2.0));
+
+	//Move to the left
+	let newPosition = enemy.newPosition(0.5 /* duration in second */, Math.PI);
+
+	TestSimpleEnemy.util.assertEqualFloat(0.5, newPosition.getX());
+	TestSimpleEnemy.util.assertEqualFloat(2.0, newPosition.getY());
+}
+
+{ //Test update
+	let target = {
+		getPosition() {
+			return Vector2D.create(1.0, 2.0);
+		}
+	};
+	let machineGun = {};
+	let drawObject = {};
+
+	let enemy = SimpleEnemy.create(target, machineGun, drawObject);
+	enemy.setPosition(Vector2D.create(-1.0, 2.0));
+	enemy.update(1.0 /* duration in second */);
+
+	TestSimpleEnemy.util.assertEqualFloat(0.0, enemy.getPosition().getX());
+}
