3ae62a22636e6e95341afe00fe614fec42beb926
BrainVM <sugimoto.massayoshi@gmail.com>
2017-02-04 22:11:14 +0900

    Remove 'placeOn' function

--- src/geometry/GameSpacePositionableComposite.js
+++ src/geometry/GameSpacePositionableComposite.js
@@ -20,11 +20,4 @@ const GameSpacePositionableComposite = (state) => ({
 		return this;
 	},
 
-	placeOn(canvasContext) {
-		let screenPosition = state.getPosition().scalarMultiply(PIXEL_PER_METER);
-		canvasContext.translate(screenPosition.getX(), screenPosition.getY());
-		canvasContext.rotate(state.getDirection());
-		return this;
-	}
-
 })
