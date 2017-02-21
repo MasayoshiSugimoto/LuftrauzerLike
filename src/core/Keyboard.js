7f6d62301497c4870001cfe07ddf21b058e3e466
Sugimoto Masayoshi <sugimoto.massayoshi@gmail.com>
2017-02-21 22:08:53 +0900

    Move the keyboard key code to another file.

--- src/core/Keyboard.js
+++ src/core/Keyboard.js
@@ -1,10 +1,5 @@
 "use strict";
 
-const KEYBOARD_KEY_E      = 87;
-const KEYBOARD_KEY_A      = 65;
-const KEYBOARD_KEY_D      = 68;
-const KEYBOARD_KEY_SPACE  = 32;
-
 const Keyboard = {
 
 	setup(initializer) {
---
+++ src/core/KeyboardConsts.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const KEYBOARD_KEY_E      = 87;
+const KEYBOARD_KEY_A      = 65;
+const KEYBOARD_KEY_D      = 68;
+const KEYBOARD_KEY_SPACE  = 32;
+
