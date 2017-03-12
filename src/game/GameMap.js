7b5c94ce1d6ce24fb32c3da352135669cf03e1f6
Sugimoto Masayoshi <sugimoto.massayoshi@gmail.com>
2017-03-12 13:45:10 +0900

    Add GameMap class

---
+++ src/game/GameMap.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const GAME_MAP_HALF_SIZE_METER = 10.0;
+
+const GameMap = () => {
+  return {
+    getWidthMeter() {
+      return 2 * GAME_MAP_HALF_SIZE_METER;
+    },
+    getHeightMeter() {
+      return 2 * GAME_MAP_HALF_SIZE_METER;
+    },
+    getSize() {
+      return Vector2D.create(this.getWidthMeter(), this.getHeightMeter());
+    },
+    getPosition() {
+      return Vector2D.create(-GAME_MAP_HALF_SIZE_METER, -GAME_MAP_HALF_SIZE_METER);
+    },
+    isInside(vector) {
+      return vector.getX() >= -GAME_MAP_HALF_SIZE_METER
+          && vector.getX() <= GAME_MAP_HALF_SIZE_METER
+          && vector.getY() >= -GAME_MAP_HALF_SIZE_METER
+          && vector.getY() <= GAME_MAP_HALF_SIZE_METER;
+    },
+  };
+};
+
