685d74867baff9ef124fecc523811f5a3dac0732
Masayoshi <sugimoto.massayoshi@gmail.com>
2021-02-27 11:39:33 +0900

    Refactoring

--- src/graphics/drawObjects/DrawObjectManager.js
+++ src/graphics/drawObjects/DrawObjectManager.js
@@ -4,75 +4,68 @@ const DrawObjectManager = {
 
   create() {
 
-    return Object.assign(
-      {
-        drawObjects: []
-      },
-      {
+    return {
+			drawObjects: [],
 
-        add(drawObject) {
-          this.drawObjects.push(drawObject);
-          return this;
-        },
+			add(drawObject) {
+				this.drawObjects.push(drawObject);
+				return this;
+			},
 
-        length() {
-          return this.drawObjects.length;
-        },
+			length() {
+				return this.drawObjects.length;
+			},
 
-        get(index) {
-          return this.drawObjects[index];
-        },
+			get(index) {
+				return this.drawObjects[index];
+			},
 
-        remove(drawObject) {
-          this.drawObjects = this.drawObjects.filter( function(element) {
-            return element != drawObject;
-          } );
-          return this;
-        },
+			remove(drawObject) {
+				this.drawObjects = this.drawObjects
+					.filter(element => element != drawObject);
+				return this;
+			},
 
-        clean() {
-          this.drawObjects = this.drawObjects.filter( (drawObject) => {
-            return !drawObject.toDelete();
-          } );
-          return this;
-        },
+			clean() {
+				this.drawObjects = this.drawObjects.filter( (drawObject) => {
+					return !drawObject.toDelete();
+				} );
+				return this;
+			},
 
-        draw(camera, elapsedTimeSecond) {
-          //Clean the list of draw objects
-          this.clean();
+			draw(camera, elapsedTimeSecond) {
+				//Clean the list of draw objects
+				this.clean();
 
-          let canvasContext = camera.getCanvas().getContext();
-          canvasContext.save();
+				let canvasContext = camera.getCanvas().getContext();
+				canvasContext.save();
 
-          //Move the context to the camera
-          canvasContext.translate(
-              camera.getCanvasTranslation().getX(),
-              camera.getCanvasTranslation().getY());
+				//Move the context to the camera
+				canvasContext.translate(
+						camera.getCanvasTranslation().getX(),
+						camera.getCanvasTranslation().getY());
 
-          this.drawAllObjects(canvasContext, elapsedTimeSecond);
+				this.drawAllObjects(canvasContext, elapsedTimeSecond);
 
-          canvasContext.restore();
+				canvasContext.restore();
 
-          return this;
-        },
+				return this;
+			},
 
-        drawAllObjects(canvasContext, elapsedTimeSecond) {
+			drawAllObjects(canvasContext, elapsedTimeSecond) {
 
-          this.drawObjects.forEach( function(drawObject) {
-            canvasContext.save();
-            let position = drawObject.getScreenPosition();
-            canvasContext.translate(position.getX(), position.getY());
-            canvasContext.rotate(drawObject.getDirection());
-            drawObject.draw(canvasContext, elapsedTimeSecond);
-            canvasContext.restore();
-          } );
+				this.drawObjects.forEach(drawObject => {
+					canvasContext.save();
+					let position = drawObject.getScreenPosition();
+					canvasContext.translate(position.getX(), position.getY());
+					canvasContext.rotate(drawObject.getDirection());
+					drawObject.draw(canvasContext, elapsedTimeSecond);
+					canvasContext.restore();
+				});
 
-          return this;
-        }
-
-      }
-
-    );
+				return this;
+			}
+		}
   }
 
 };
