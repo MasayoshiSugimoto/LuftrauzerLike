644a268805c69a56d6638a5be6a47974cb2b973c
Masayoshi Sugimoto <sugimoto.massayoshi@gmail.com>
2021-08-26 00:43:16 +0900

    Remove old code.

--- src/core/MathUtil.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function MathUtil() {}
-
-MathUtil.EPSILON = 0.00001;
-
-MathUtil.clamp = (minValue, value, maxValue) => {
-  return Math.min(maxValue, Math.max(value, minValue));
-};
--- src/graphics/drawObjects/EmptyDrawObject.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-//This empty draw object does nothing.
-const EmptyDrawObject = {
-  className: "EmptyDrawObject",
-  update(elapsedTimeSecond) { return this; },
-  draw() { /* Do nothing */ },
-  toDelete() { }
-}
